f5d889a23128e47260c1fa108e920980655f4fc1
Phillip Whelan <phil@calyptia.com>
2024-02-19 18:07:14 -0300

    in_http: parse msgpack payloads emitted by out_http.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- plugins/in_http/http_prot.c
+++ plugins/in_http/http_prot.c
@@ -662,6 +662,13 @@ static int process_payload(struct flb_http *ctx, struct http_conn *conn,
             return -1;
         }
     }
+    else if (type == HTTP_CONTENT_MSGPACK) {
+        ret = parse_payload_msgpack(ctx, tag, request->data.data, request->data.len);
+        if (ret != 0) {
+            send_response(conn, 400, "error: invalid msgpack payload\n");
+            return -1;
+        }
+    }
 
     return 0;
 }
